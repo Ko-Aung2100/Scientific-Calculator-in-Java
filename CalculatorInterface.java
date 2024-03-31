39264ae5afdb075baf501435d606e502ec75fc74
Ko-Aung2100 <aungkaunghtetps2100@gmail.com>
2024-04-01 06:09:06 +0700

    Remove all files from master branch

--- CalculatorInterface.java
+++
@@ -1,6 +0,0 @@
-public interface CalculatorInterface{
-    public double addition(double[] arr);
-    public double subtraction(double[] arr);
-    public double multiplication(double[] arr);
-    public double division(double[] arr);
-}
